159226613ec13a0aa3720fea25cf9d3aeee24cc9
Christopher Fuller <chris@chrisfuller.me>
2019-08-25 15:03:57 -0700

    Update step script

--- step.sh
+++ step.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
-set -ex
+#!/usr/bin/env zsh -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+set -o pipefail
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+RESULT_BUNDLE_PATH="$(mktemp -d)/Test.xcresult"
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+env NSUnbufferedIO=YES \
+xcodebuild \
+-workspace "${xc_option_workspace}" \
+-scheme "${xc_option_scheme}" \
+-sdk "${xc_option_sdk}" \
+-destination "${xc_option_destination}" \
+-resultBundlePath "${RESULT_BUNDLE_PATH}" \
+"${xcodebuild_actions}" \
+| tee "${raw_xcodebuild_output}" \
+| "${xcodebuild_output_formatter}"
+
+cp -a "${RESULT_BUNDLE_PATH}" "${BITRISE_TEST_DEPLOY_DIR}/${BITRISE_SCHEME}/"
