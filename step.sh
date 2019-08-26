777f5be885d843396da5da5e8983b5656bc79184
Christopher Fuller <chris@chrisfuller.me>
2019-08-26 08:35:03 -0700

    Refactor inputs and add output for result bundle

--- step.sh
+++ step.sh
@@ -2,17 +2,17 @@
 
 set -o pipefail
 
-RESULT_BUNDLE_PATH="$(mktemp -d)/Test.xcresult"
+XCRESULT_BUNDLE_PATH="$(mktemp -d)/Test.xcresult"
 
 env NSUnbufferedIO=YES \
 xcodebuild \
--workspace "${xc_option_workspace}" \
--scheme "${xc_option_scheme}" \
--sdk "${xc_option_sdk}" \
--destination "${xc_option_destination}" \
--resultBundlePath "${RESULT_BUNDLE_PATH}" \
+-workspace "${xcodebuild_workspace}" \
+-scheme "${xcodebuild_scheme}" \
+-sdk "${xcodebuild_sdk}" \
+-destination "${xcodebuild_destination}" \
+-resultBundlePath "${XCRESULT_BUNDLE_PATH}" \
 "${xcodebuild_actions}" \
 | tee "${raw_xcodebuild_output}" \
 | "${xcodebuild_output_formatter}"
 
-cp -a "${RESULT_BUNDLE_PATH}" "${BITRISE_TEST_DEPLOY_DIR}/${BITRISE_SCHEME}/"
+envman add --key XCRESULT_BUNDLE_PATH --value "${XCRESULT_BUNDLE_PATH}"
